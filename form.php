41b9087d53e0c4d01bbe5723162901ba48c96a2b
Zac <a19860101@gmail.com>
2021-01-24 15:51:49 +0800

    202210124

--- form.php
+++ form.php
@@ -7,9 +7,31 @@
 </head>
 <body>
     <form action="res.php" method="post">
-        <input type="text" name="user">
-        <input type="text" name="mail">
-        <input type="text" name="phone">
+        <div>
+            <input type="text" name="user">
+        </div>
+        <div>
+            <input type="text" name="mail">
+        </div>
+        <div>
+            <input type="text" name="phone">
+        </div>
+        <div>
+            <input type="radio" name="gender" value="男"> 男
+            <input type="radio" name="gender" value="女"> 女
+        </div>
+        <div>
+            <select name="edu" id="">
+                <option value="國小">國小</option>
+                <option value="國中">國中</option>
+                <option value="高中職">高中職</option>
+                <option value="大專院校">大專院校</option>
+                <option value="研究所以上">研究所以上</option>
+            </select>
+        </div>
+        <div>
+            <textarea name="content" id="" cols="30" rows="10"></textarea>
+        </div>
         <input type="submit" value="送出">
     </form>
 </body>
--- res.php
+++ res.php
@@ -1,9 +1,23 @@
 <?php
     //全域變數
-    echo $_POST;
+    // echo $_POST;
     // var_dump($_POST);
-    echo $_POST["user"];
+    // $user = $_POST["user"];
+    // $mail = $_POST["mail"];
+    // $phone = $_POST["phone"];
+    // $gender = $_POST["gender"];
+    // $edu = $_POST["edu"];
+    // $content = $_POST["content"];
+    extract($_POST);
+    echo $user;
     echo "<br>";
-    echo $_POST["mail"];
+    echo $mail;
     echo "<br>";
-    echo $_POST["phone"];
+    echo $phone;
+    echo "<br>";
+    echo $gender;
+    echo "<br>";
+    echo $edu;
+    echo "<br>";
+    echo $content;
+    echo "<br>";
