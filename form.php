314bebfc0f479c095830bca6ffeefe0b719cb185
Zac <a19860101@gmail.com>
2021-01-24 16:12:38 +0800

    20210124

--- form.php
+++ form.php
@@ -8,19 +8,24 @@
 <body>
     <form action="res.php" method="post">
         <div>
+            <label for="user">姓名</label>
             <input type="text" name="user">
         </div>
         <div>
+            <label for="mail">Mail</label>
             <input type="text" name="mail">
         </div>
         <div>
+            <label for="phone">電話</label>
             <input type="text" name="phone">
         </div>
         <div>
+            <label for="gender">性別</label>
             <input type="radio" name="gender" value="男"> 男
             <input type="radio" name="gender" value="女"> 女
         </div>
         <div>
+            <label for="edu">學歷</label>
             <select name="edu" id="">
                 <option value="國小">國小</option>
                 <option value="國中">國中</option>
@@ -30,12 +35,14 @@
             </select>
         </div>
         <div>
+            <label for="interest">興趣</label>
             <input type="checkbox" name="interest[]" value="吃">
             <input type="checkbox" name="interest[]" value="喝">
             <input type="checkbox" name="interest[]" value="玩">
             <input type="checkbox" name="interest[]" value="樂">
         </div>
         <div>
+            <label for="content">備註</label>
             <textarea name="content" id="" cols="30" rows="10"></textarea>
         </div>
         <input type="submit" value="送出">
