797cba0446334a7dc55a8f4b7557ac31d28f0870
Zac <a19860101@gmail.com>
2021-01-31 13:47:16 +0800

    20210131

--- database/index.php
+++ database/index.php
@@ -3,5 +3,15 @@
     $sql = "SELECT * FROM students";
     $result = mysqli_query($conn,$sql); 
     // var_dump($result);
-    $row = mysqli_fetch_assoc($result);
-    var_dump($row["name"]);
+    // $row = mysqli_fetch_assoc($result);
+    // $row = mysqli_fetch_array($result);
+    // $row = mysqli_fetch_row($result);
+    // var_dump($row);
+
+    while($row = mysqli_fetch_assoc($result)){
+        echo $row["name"];
+        echo "<br>";
+        echo $row["mail"];
+        echo "<br>";
+    }
+
