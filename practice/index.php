e3a383723f39beb4c376b04758f089a4a6b30e20
Zac <a19860101@gmail.com>
2021-02-07 15:50:38 +0800

    20210207

--- practice/index.php
+++ practice/index.php
@@ -11,76 +11,81 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .discount {
-            color: #f00;
-            font-size: 30px;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>產品管理系統</h1>
-    <nav>
-        <a href="create.php">新增商品</a>
-    </nav>
-    <table border="1">
-        <tr>
-            <td>#</td>
-            <td>產品名稱</td>
-            <td>價格</td>
-            <td>折扣後價格</td>
-            <td>上架</td>
-            <td>數量</td>
-            <td>折扣</td>
-            <td>動作</td>
-        </tr>
-        <?php while($row = mysqli_fetch_assoc($result)){ ?>
-        <tr>
-            <td><?php echo $row["id"];?></td>
-            <td><?php echo $row["title"];?></td>
-            <td><?php echo $row["price"];?></td>
-            <td>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>產品管理系統</h1>
+            <a href="create.php" class="btn btn-primary">新增商品</a>
+        </div>
+        <div class="col-12">
+            <table class="table">
+            <tr>
+                <td>#</td>
+                <td>產品名稱</td>
+                <td>價格</td>
+                <td>折扣後價格</td>
+                <td>上架</td>
+                <td>數量</td>
+                <td>折扣</td>
+                <td>動作</td>
+            </tr>
+            <?php while($row = mysqli_fetch_assoc($result)){ ?>
+            <tr>
+                <td><?php echo $row["id"];?></td>
+                <td><?php echo $row["title"];?></td>
+                <td><span class="badge bg-secondary"><?php echo $row["price"];?></span></td>
+                <td>
+                    <?php
+                        if($row["discount"] < 1){
+                            echo "<span class='badge bg-danger'>".$row["price"] * $row["discount"]."</span>";
+                        }else{
+                            echo "<span class='badge bg-secondary'>".$row["price"] * $row["discount"]."</span>";
+                        }
+                    ?>
+                </td>
+                <td>
+                    <?php echo $row["sale"] == 1 ? "是":"否"; ?>
+                </td>
+                <td><?php echo $row["quantity"];?></td>
+                <td>
                 <?php
-                    if($row["discount"] < 1){
-                        echo "<span class='discount'>".$row["price"] * $row["discount"]."</span>";
-                    }else{
-                        echo $row["price"] * $row["discount"];
+                    switch($row["discount"]){
+                        case 1:
+                            echo "無折扣";
+                            break;
+                        case 0.9:
+                            echo "九折";
+                            break;
+                        case 0.8:
+                            echo "八折";
+                            break;
+                        case 0.7:
+                            echo "七折";
+                            break;
                     }
+                
                 ?>
-            </td>
-            <td>
-                <?php echo $row["sale"] == 1 ? "是":"否"; ?>
-            </td>
-            <td><?php echo $row["quantity"];?></td>
-            <td>
-            <?php
-                switch($row["discount"]){
-                    case 1:
-                        echo "無折扣";
-                        break;
-                    case 0.9:
-                        echo "九折";
-                        break;
-                    case 0.8:
-                        echo "八折";
-                        break;
-                    case 0.7:
-                        echo "七折";
-                        break;
-                }
-            
-            ?>
-            </td>
-            <td>
-                <form action="delete.php" method="post">
-                    <input type="hidden" name="id" value="<?php echo $row["id"];?>">
-                    <input type="submit" value="刪除商品" onclick="return confirm('確認刪除？')">
-                </form>
+                </td>
+                <td>
+                    <form action="delete.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                        <input type="submit" value="刪除商品" onclick="return confirm('確認刪除？')" class="btn btn-danger">
+                    </form>
 
-                <a href="edit.php?id=<?php echo $row["id"];?>">編輯商品</a>
-            </td>
-        </tr>
-        <?php } ?>
-    </table>
+                    <a href="edit.php?id=<?php echo $row["id"];?>" class="btn btn-success">編輯商品</a>
+                </td>
+            </tr>
+            <?php } ?>
+        </table>
+        </div>
+    </div>
+</div>
+    
+
+    
 </body>
 </html>
