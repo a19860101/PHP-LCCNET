b9e15edd26fb332adaa0c90403ef0250c2202477
Zac <a19860101@gmail.com>
2021-02-21 15:04:19 +0800

    20210221

--- filesystem/pdo.php
+++ filesystem/pdo.php
@@ -6,4 +6,11 @@
     $db_charset = "utf8mb4";
 
     $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-    $pdo = new PDO($dsn,$db_user,$db_pw);//建立PDO實體
+    // $pdo = new PDO($dsn,$db_user,$db_pw);//建立PDO實體
+
+    //例外處理 錯誤觸理
+    try {
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
