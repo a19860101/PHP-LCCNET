6579a6981d5febad9620e4508be8bd0131094eb2
Zac <a19860101@gmail.com>
2021-02-21 10:11:53 +0800

    20210221

---
+++ filesystem/upload.php
@@ -0,0 +1,12 @@
+<?php
+    // var_dump($_FILES["gallery"]["name"]);
+    echo $_FILES["gallery"]["name"];
+    echo "<br>";
+    echo $_FILES["gallery"]["size"];
+    echo "<br>";
+    echo $_FILES["gallery"]["type"];
+    echo "<br>";
+    echo $_FILES["gallery"]["tmp_name"];
+    echo "<br>";
+    echo $_FILES["gallery"]["error"];
+    echo "<br>";
