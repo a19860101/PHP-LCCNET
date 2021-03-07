bf650324c51ec6e2b48067b55eb03a6f05ff2f19
Zac <a19860101@gmail.com>
2021-03-07 16:36:34 +0800

    20210307

--- member/store.php
+++ member/store.php
@@ -3,6 +3,19 @@
     try {
         require_once("pdo.php");
         extract($_POST);
+
+        $sql_check = "SELECT * FROM users WHERE user = ?";
+        $stmt_check = $pdo->prepare($sql_check);
+        $stmt_check->execute([$user]);
+
+        // echo $stmt_check -> rowCount();
+
+        if($stmt_check -> rowCount() > 0){
+            echo "帳號已存在，請重新申請!!";
+            header("refresh:1;url=register.php");
+            return ;
+        }
+
         $sql = "INSERT INTO users(user,pw,created_at)VALUES(?,?,?)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$user,$pw,$now]);
