6a39953ce6d42ae07739f18fcea4c3250b17d6b2
Zac <a19860101@gmail.com>
2021-03-14 14:35:02 +0800

    20210314

--- blog/function.php
+++ blog/function.php
@@ -23,9 +23,31 @@
         extract($request);
         $user_id = 1;
         $category_id = 1;
-        $sql = "INSERT INTO posts(title,content,user_id,category_id,created_at,updated_at)VALUES(?,?,?,?,?,?)";
+        //key
+        $k = array_keys($request);
+        array_push($k,"user_id","category_id","created_at","updated_at");
+        $k = implode(",",$k);
+        // var_dump($k);
+        // echo $k;
+
+        //value
+        $v = explode(",",implode(",",$request));
+        array_push($v,$user_id,$category_id,$now,$now);
+        // var_dump($v);
+
+        //問號
+        $q = implode(",",array_map(function(){
+            return "?";
+        },$v));
+        // echo $q;
+
+        // $sql = "INSERT INTO posts(title,content,user_id,category_id,created_at,updated_at)VALUES(?,?,?,?,?,?)";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt -> execute([$title,$content,$user_id,$category_id,$now,$now]);
+
+        $sql = "INSERT INTO posts(".$k.")VALUES(".$q.")";
         $stmt = $pdo->prepare($sql);
-        $stmt -> execute([$title,$content,$user_id,$category_id,$now,$now]);
+        $stmt->execute($v);
     }
     function deletePost(){}
     function updatePost(){}
