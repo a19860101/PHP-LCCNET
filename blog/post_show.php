e2a60494a09320429c6c1313380d03ee7be9d3a4
Zac <a19860101@gmail.com>
2021-03-28 09:56:52 +0800

    20210328

--- blog/post_show.php
+++ blog/post_show.php
@@ -27,13 +27,16 @@
             更新時間<?php echo $post["updated_at"];?>
         </div>
         <a href="index.php">文章列表</a>
-        <?php if($post["user_id"] == $_SESSION["AUTH"]["id"]){ ?>
+        <?php 
+            if(isset($_SESSION["AUTH"])){
+                if($post["user_id"] == $_SESSION["AUTH"]["id"]){ 
+        ?>
         <a href="post_edit.php?id=<?php echo $post["id"];?>">編輯文章</a>
         <form action="post_delete.php" method="post">
             <input type="hidden" name="id" value="<?php echo $post["id"];?>">
             <input type="submit" value="刪除文章" onclick="return confirm('確認刪除文章？')">
         </form>
-        <?php } ?>
+        <?php }} ?>
     </div>
 </body>
 </html>
