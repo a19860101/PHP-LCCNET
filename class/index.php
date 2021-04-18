c9f176ab229bce3667fb95b31de6fe9c96b8c8cf
Zac <a19860101@gmail.com>
2021-04-18 16:36:41 +0800

    20210418

---
+++ class/index.php
@@ -0,0 +1,31 @@
+<?php
+    class USER {
+        public $asdf;
+        function asdf(){
+            
+        }
+    }
+    class Person {
+        public $hairColor = 'black';
+        public $skin = 'black';
+        public $eye;
+
+        function run(){}
+        function test1(){}
+        function test2(){}
+        function test3(){}
+        function test4(){}
+    }
+    class Dog {
+        public $hairColor;
+        public $hairColor;
+        public $hairColor;
+        public $hairColor;
+        function run(){}
+    }
+
+    $john = new Person;
+    $john->run();
+
+    $lucky = new Dog;
+    $lucky->run();
